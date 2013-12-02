2bdb6274b97a187c30f3776d29a2f69953d09377
Michael Kevin <mikeru3000@yahoo.com>
2013-12-03 00:40:03 +0700

    Tugas 3 Fixed

--- Tugas 3.php
+++ Tugas 3.php
@@ -10,23 +10,24 @@
 	<div data-role="fieldcontain">
 		<?php 
 		function test_xml(){
-				$f1 = "http://localhost/a/habib-andy/index.php/ws/menu/semua";
-				$f2 = "http://localhost/a/II3160-18211017-18211043/database_collection/xmldb.xml";
-				$f3 = "http://localhost/a/BernadetteVina/csv.php";
-				$f4 = "http://localhost/a/II3160-18211003-18211050/menu.xml";
-				$f5 = "http://localhost/a/II3160--Pemrograman-Integratif-/DaftarIdol.xml";
-				$f6 = "http://localhost/a/II3160-Tugas1-Tugas2/tab2.xml";
-				$f7 = "http://localhost/a/IPT-Assignments/data2.xml";
-				$f8 = "http://localhost/a/pemrograman_integratif/output.xml";
-				$f9 = "http://localhost/a/Workspace/Menu.xml";
-				$f10 = "http://localhost/a/progin/contoh.xml";
-				$f11 = "http://localhost/a/Progint/data/xml/1.xml";
-				$f12 = "http://localhost/a/BernadetteVina/DataXML.xml";
-				$f13 = "http://localhost/a/testPHP2/test.xml";
-				$f14 = "http://localhost/a/tugas-2-pemrograman-integratif/data3.xml";
-				$f15 = "http://localhost/a/web-service/datasiswa.xml";
+				$f1 = "http://localhost/habib-andy/index.php/ws/menu/semua";
+				$f2 = "http://localhost/II3160-Tugas-18211011-18211053/index.php/search/korans";
+				$f3 = "http://localhost/BernadetteVina/csv.php";
+				$f4 = "http://localhost/ningenis/II3160-Progin-18211002-18211033/getSelf.php?input=semua";
+				$f5 = "http://localhost/pemrograman-integratif/artis.php";
+				$f6 = "http://localhost/pemrograman_integratif/informasi_xml.php?nim=all";
+				$f7 = "http://localhost/18211037/RestWebService/index.php/api/Liverpool/players/format/xml";
+				$f8 = "http://localhost/II3160-18211017-18211043/index.php/Api/xml_from_sql";
+				$f9 = "http://localhost/II3160-18211013-18211047/xmlconvert.php/pegawai";
+				$f10 = "http://localhost/Progint-yogidanang/?kolom=all";
+				$f11 = "http://localhost/18211010-18211035/searchmhs.php?tag=all";
+				$f12 = "http://localhost/pradita72/II3160--Pemrograman-Integratif-/ShowIdol.php?show=Info+Idol";
+				$f13 = "http://localhost/II3160-Pemograman-Integratif/show.php?data=makanan.xml";
+				$f14 = "http://localhost/18211014-dan-18211029/index2.php?state=state1&submit2=Submit";
+				$f15 = "http://localhost/II3160-18211003-18211050/punya_sendiri/?search=menu";
+				$f16 = "http://localhost/Progint/xml.php";
 			echo "<h1> Mengambil XML... </h1>";
-			for ($i=1; $i<16; $i++){
+			for ($i=1; $i<17; $i++){
 				$nf="f".$i;
 				echo "Tugas Teman $i"; 
 				$temp = simplexml_load_file($$nf);
